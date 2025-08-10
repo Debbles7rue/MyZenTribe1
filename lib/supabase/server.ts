a1a49f5ad140de9647a1f0b0870592e89ffe277b
MyZenTribe <debbles7rue@gmail.com>
2025-08-10 15:57:36 -0500

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -7,11 +7,7 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-      },
+      cookies: { get(name: string) { return cookieStore.get(name)?.value; } },
     }
   );
 }
