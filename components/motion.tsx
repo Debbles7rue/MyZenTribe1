fa6789bd06d9944cd288699994ee91c33ae7fd00
MyZenTribe <debbles7rue@gmail.com>
2025-08-10 16:23:11 -0500

    Create motion.tsx

---
+++ components/motion.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { motion } from "framer-motion";
+import type { ComponentProps } from "react";
+
+export function MotionDiv(props: ComponentProps<typeof motion.div>) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 6 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4 }}
+      {...props}
+    />
+  );
+}
