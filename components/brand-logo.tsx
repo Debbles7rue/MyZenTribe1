ae478214aeac6499eecc13e557dfc68337071349
MyZenTribe <debbles7rue@gmail.com>
2025-08-10 16:40:27 -0500

    Update brand-logo.tsx

--- components/brand-logo.tsx
+++ components/brand-logo.tsx
@@ -1,12 +1,12 @@
 export default function BrandLogo() {
   return (
     <div className="flex items-center gap-3">
-      <Image
-        src="/logo.svg"   // <-- if you uploaded PNG, change to /logo.png
+      <img
+        src="/logo.svg"   // change to /logo.png if that’s your file
         alt="MyZenTribe logo"
         width={40}
         height={40}
-        priority
+        style={{ display: 'block' }}
       />
       <span className="text-2xl font-semibold italic">
         My<span className="not-italic">Zen</span><span className="italic">Tribe</span>
