9c784dc28efd590f4c365bbc1cc030d6660a178f
MyZenTribe <debbles7rue@gmail.com>
2025-08-15 12:26:06 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
 import "./globals.css";
 import SiteHeader from "@/components/SiteHeader";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
-        <SiteHeader />
-        <div className="page-wrap">{children}</div>
+        <div className="page-wrap">
+          <SiteHeader />
+          {children}
+        </div>
       </body>
     </html>
   );
