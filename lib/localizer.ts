cdd91973e693ad1508dd96ed6728b53cd703a562
MyZenTribe <debbles7rue@gmail.com>
2025-08-15 16:36:27 -0500

    Update localizer.ts

--- lib/localizer.ts
+++ lib/localizer.ts
@@ -1,6 +1,4 @@
-import {
-  dateFnsLocalizer,
-} from "react-big-calendar";
+import { dateFnsLocalizer } from "react-big-calendar";
 import { format, parse, startOfWeek, getDay } from "date-fns";
 import enUS from "date-fns/locale/en-US";
 
