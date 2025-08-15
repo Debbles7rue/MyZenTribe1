570dad4c23fc3d57681cc711b800f07ce31be795
MyZenTribe <debbles7rue@gmail.com>
2025-08-15 16:35:40 -0500

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -9,16 +9,16 @@ export type DBEvent = {
   visibility: Visibility;
   created_by: string;
   location: string | null;
-  latitude: number | null;
-  longitude: number | null;
-  rrule: string | null;
-  event_type: string | null;
-  rsvp_public: boolean | null;
-  community_id: string | null;
-  created_at: string;
+  latitude?: number | null;
+  longitude?: number | null;
+  rrule?: string | null;
+  event_type?: string | null;
+  rsvp_public?: boolean | null;
+  community_id?: string | null;
+  created_at?: string;
 
   image_path: string | null;
-  source: 'personal' | 'business';
+  source?: 'personal' | 'business' | null;
 
   status?: 'scheduled' | 'cancelled' | null;
   cancellation_reason?: string | null;
