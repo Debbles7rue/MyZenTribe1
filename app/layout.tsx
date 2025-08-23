1b719113fb05219ee7cf523443f626679179853d
MyZenTribe <debbles7rue@gmail.com>
2025-08-22 20:59:11 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import SiteHeader from "@/components/SiteHeader";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 export const metadata: Metadata = {
   title: "MyZenTribe",
