8baa03da095546be12d1885762b68c26d7bcc5f3
MyZenTribe <debbles7rue@gmail.com>
2025-08-31 19:00:38 -0500

    Create page.tsx

---
+++ app/demo/collab-posts/page.tsx
@@ -0,0 +1,8 @@
+// app/demo/collab-posts/page.tsx
+import DemoCollabPosts from "@/components/DemoCollabPosts";
+
+export const dynamic = "force-dynamic";
+
+export default function Page() {
+  return <DemoCollabPosts />;
+}
