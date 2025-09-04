18eff83b4988b2b981affb3f76c04a47b4f22e84
MyZenTribe <debbles7rue@gmail.com>
2025-09-03 20:57:55 -0500

    Update page.tsx

--- app/communities/page.tsx
+++ app/communities/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
-export default function CommunitiesRedirect() {
-  redirect('/communities');
+export default function CommunityDetailRedirect({ params }: { params: { id: string } }) {
+  redirect(`/communities/${params.id}`);
   return null;
 }
