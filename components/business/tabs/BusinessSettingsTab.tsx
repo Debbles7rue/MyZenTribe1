980dbe0a5d7acf9ee8e72968fe098a007e258a80
MyZenTribe <debbles7rue@gmail.com>
2025-09-15 17:20:47 -0500

    Create BusinessSettingsTab.tsx

---
+++ components/business/tabs/BusinessSettingsTab.tsx
@@ -0,0 +1,6 @@
+// components/business/tabs/BusinessSettingsTab.tsx
+'use client';
+
+export default function BusinessSettingsTab({ businessId }: { businessId: string }) {
+  return <div className="text-gray-600">Settings coming soon...</div>;
+}
