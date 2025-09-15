b8e12cb1bfa3dd0bc1c14bfec6f8a1ea73f207a5
MyZenTribe <debbles7rue@gmail.com>
2025-09-15 17:20:20 -0500

    Create BusinessServicesTab.tsx

---
+++ components/business/tabs/BusinessServicesTab.tsx
@@ -0,0 +1,6 @@
+// components/business/tabs/BusinessServicesTab.tsx
+'use client';
+
+export default function BusinessServicesTab({ businessId }: { businessId: string }) {
+  return <div className="text-gray-600">Services coming soon...</div>;
+}
