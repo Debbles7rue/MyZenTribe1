04651cdc66e8919f1299e84df1a9f5a2f7af64ff
MyZenTribe <debbles7rue@gmail.com>
2025-09-15 17:18:58 -0500

    Create BusinessSocialTab.tsx

---
+++ components/business/tabs/BusinessSocialTab.tsx
@@ -0,0 +1,6 @@
+// components/business/tabs/BusinessSocialTab.tsx
+'use client';
+
+export default function BusinessSocialTab({ businessId }: { businessId: string }) {
+  return <div className="text-gray-600">Social links coming soon...</div>;
+}
