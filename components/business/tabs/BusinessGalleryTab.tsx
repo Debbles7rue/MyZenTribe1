1fd550105e691b23c41346d733e8aa0193b9a128
MyZenTribe <debbles7rue@gmail.com>
2025-09-15 17:18:19 -0500

    Create BusinessGalleryTab.tsx

---
+++ components/business/tabs/BusinessGalleryTab.tsx
@@ -0,0 +1,6 @@
+// components/business/tabs/BusinessGalleryTab.tsx
+'use client';
+
+export default function BusinessGalleryTab({ businessId }: { businessId: string }) {
+  return <div className="text-gray-600">Gallery coming soon...</div>;
+}
