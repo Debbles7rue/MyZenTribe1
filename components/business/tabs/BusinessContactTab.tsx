84fb94506cd9afb2ac2cb91af5b8da05f5ff563e
MyZenTribe <debbles7rue@gmail.com>
2025-09-15 17:17:48 -0500

    Create BusinessContactTab.tsx

---
+++ components/business/tabs/BusinessContactTab.tsx
@@ -0,0 +1,6 @@
+// components/business/tabs/BusinessContactTab.tsx
+'use client';
+
+export default function BusinessContactTab({ businessId }: { businessId: string }) {
+  return <div className="text-gray-600">Contact settings coming soon...</div>;
+}
