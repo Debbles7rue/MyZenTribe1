3878dc2d82343f18e681ca529285e4c935de81c2
MyZenTribe <debbles7rue@gmail.com>
2025-09-15 17:19:45 -0500

    Create BusinessHoursTab.tsx

---
+++ components/business/tabs/BusinessHoursTab.tsx
@@ -0,0 +1,6 @@
+// components/business/tabs/BusinessHoursTab.tsx
+'use client';
+
+export default function BusinessHoursTab({ businessId }: { businessId: string }) {
+  return <div className="text-gray-600">Hours settings coming soon...</div>;
+}
