500827d2570d96b242d05c04df1891241dac9b60
MyZenTribe <debbles7rue@gmail.com>
2025-09-16 08:51:40 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const path = require("path");
 
 module.exports = {
+  output: 'standalone', // or 'export' depending on your setup
   reactStrictMode: true,
   // Keep builds moving while we stabilize types/lint (optional; remove later if you like)
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-
   webpack: (config) => {
     // Alias 'ci-info' to our local stub to satisfy Next telemetry import
     config.resolve.alias["ci-info"] = path.resolve(__dirname, "stubs/ci-info.js");
